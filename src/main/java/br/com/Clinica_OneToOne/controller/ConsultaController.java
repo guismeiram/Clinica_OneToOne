c5c4a791336c9b174f66b3d475839da6f146e945
guismeiram <boralagui@outlook.com>
2022-07-03 17:28:08 -0300

    commitando controllers

--- src/main/java/br/com/Clinica_OneToOne/controller/ConsultaController.java
+++ src/main/java/br/com/Clinica_OneToOne/controller/ConsultaController.java
@@ -1,4 +1,79 @@
 package br.com.Clinica_OneToOne.controller;
 
+import br.com.Clinica_OneToOne.dto.ConsultaDTO;
+import br.com.Clinica_OneToOne.exception.ClinicaNotFoundException;
+import br.com.Clinica_OneToOne.models.Consulta;
+import br.com.Clinica_OneToOne.service.ConsultaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("/api/consulta")
 public class ConsultaController {
+
+    private ConsultaService consultaService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @PostMapping
+    @Transactional
+    public ConsultaDTO createConsulta(@RequestBody ConsultaDTO consultaDTO)
+    {
+        Consulta consultaRequest = mapper.map(consultaDTO, Consulta.class);
+        //consultaRequest.getConsultorio().forEach(c ->c.setNumero(c.getNumero()));
+        //Paciente
+       consultaRequest.getPaciente().getNome();
+       //consulta
+        consultaRequest.getNumero();
+        consultaRequest.getEspecialidade();
+        consultaRequest.getData_hora();
+
+        //Grava na Memoria
+        Consulta consulta = consultaService.createConsulta(consultaRequest);
+        // convert entity to DTO
+        return mapper.map(consulta, ConsultaDTO.class);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ConsultaDTO> getPostById(@PathVariable(name = "id") Long id) throws ClinicaNotFoundException {
+        Consulta consulta = consultaService.getConsultaById(id);
+
+        // convert entity to DTO
+        ConsultaDTO consultaResponse = mapper.map(consulta, ConsultaDTO.class);
+
+        return ResponseEntity.ok().body(consultaResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable(name = "id") Long id) throws ClinicaNotFoundException {
+        consultaService.deleteConsulta(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ConsultaDTO> updateImovel(@RequestBody ConsultaDTO consultaDTO, @PathVariable long id) throws ClinicaNotFoundException {
+        Consulta consultaRequest = mapper.map(consultaDTO, Consulta.class);
+
+        Consulta consulta = consultaService.updateById(consultaRequest, id);
+
+        // convert entity to DTO
+        ConsultaDTO consultaResponse = mapper.map(consulta, ConsultaDTO.class);
+
+        return ResponseEntity.ok().body(consultaResponse);
+    }
+
 }
--- src/main/java/br/com/Clinica_OneToOne/controller/PacienteController.java
+++ src/main/java/br/com/Clinica_OneToOne/controller/PacienteController.java
@@ -1,6 +1,82 @@
 package br.com.Clinica_OneToOne.controller;
 
+import br.com.Clinica_OneToOne.dto.PacienteDTO;
+import br.com.Clinica_OneToOne.exception.ClinicaNotFoundException;
+import br.com.Clinica_OneToOne.models.Paciente;
+import br.com.Clinica_OneToOne.service.PacienteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PacienteController {
 
-    
+    @Autowired
+    private final PacienteService pacienteService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public PacienteController(PacienteService pacienteService, ModelMapper modelMapper) {
+        this.pacienteService = pacienteService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> createPaciente(@RequestBody PacienteDTO pacienteDTO) {
+        // convert DTO to entity
+        Paciente pacienteRequest = modelMapper.map(pacienteDTO, Paciente.class);
+
+        Paciente paciente = pacienteService.createPaciente(pacienteRequest);
+
+        // convert entity to DTO
+        PacienteDTO pacienteResponse = modelMapper.map(paciente, PacienteDTO.class);
+
+        return new ResponseEntity<PacienteDTO>(pacienteResponse, HttpStatus.CREATED);
+    }
+    ///////
+
+
+
+
+    @GetMapping
+    public List<PacienteDTO> getAllPaciente() {
+
+        return pacienteService.findAll().stream().map(post -> modelMapper.map(post, PacienteDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteDTO> getMedicoById(@PathVariable(name = "id") Long id) throws ClinicaNotFoundException {
+        Paciente paciente = pacienteService.getMedicoById(id);
+
+        // convert entity to DTO
+        PacienteDTO pacienteResponse = modelMapper.map(paciente, PacienteDTO.class);
+
+        return ResponseEntity.ok().body(pacienteResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePaciente(@PathVariable(name = "id") Long id) throws ClinicaNotFoundException {
+        pacienteService.deleteMedico(id);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteDTO> updateImovel(@RequestBody PacienteDTO pacienteDTO, @PathVariable long id) throws ClinicaNotFoundException {
+        Paciente pacienteRequest = modelMapper.map(pacienteDTO, Paciente.class);
+
+        Paciente paciente = pacienteService.updateById(id, pacienteRequest);
+
+        // convert entity to DTO
+        PacienteDTO pacienteResponse = modelMapper.map(paciente, PacienteDTO.class);
+
+        return ResponseEntity.ok().body(pacienteResponse);
+    }
+
 }
